7ee016549b7d4d1c6bdbd30d66672f27f43e673a
UserNameFarhad <fa85619@gmail.com>
2024-04-24 23:25:53 +0400

    Tasks added

---
+++ Gender Task 1/script.js
@@ -0,0 +1,4 @@
+const Gender = prompt('Genderin adini daxil edin');
+const ad = prompt('adinizi daxil edin');
+if (Gender === "k") {document.write(`Salam cenab ${ad}`)}
+else if (Gender === "g") {document.write(`Salam xanim ${ad}`)}
---
+++ Lesson 18 Aylar/Script.js
@@ -0,0 +1,13 @@
+const ay = +prompt('Necenci ay ?')
+if(ay === 1) {document.write('Yanvar')}
+else if (ay === 2) {document.write('Fevral')}
+else if (ay === 3) {document.write('Mart')}
+else if (ay === 4) {document.write('Aprel')}
+else if (ay === 5) {document.write('May')}
+else if (ay === 6) {document.write('Iyun')}
+else if (ay === 7) {document.write('Iyul')}
+else if (ay === 8) {document.write('Avqust')}
+else if (ay === 9) {document.write('Sentyabr')}
+else if (ay === 10) {document.write('Oktyabr')}
+else if (ay === 11) {document.write('Noyabr')}
+else if (ay === 12) {document.write('Dekabr')}
