f064e025b86b92d2ddf114198bcb262d8c8273e8
UserNameFarhad <fa85619@gmail.com>
2024-04-24 22:14:40 +0400

    Task hefte gunleri added

---
+++ Lesson 18 Heftenin gunleri/script.js
@@ -0,0 +1,8 @@
+const gun = +prompt('necenci gundur');
+if (gun === 1) { document.write('Bazar ertesi') }
+else if (gun === 2) { document.write('cersenbe axwami') }
+else if (gun === 3) { document.write('cersenbe') }
+else if (gun === 4) { document.write('cume axwami') }
+else if (gun === 5) { document.write('cume') }
+else if (gun === 6) { document.write('senbe') }
+else if (gun === 7) { document.write('bazar') }
