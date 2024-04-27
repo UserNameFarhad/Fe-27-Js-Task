bbfbd877686eccd0a782ce2c593b97047c5b3ac8
UserNameFarhad <fa85619@gmail.com>
2024-04-27 14:50:10 +0400

    User task added

---
+++ Lesson 18 Ev alisi Task/App.js
@@ -0,0 +1,6 @@
+let kv = +prompt('Kvadrati teyin edin')
+let housePrice = +prompt('qiymeti teyin edin')
+let currency = prompt('Valyutani teyin edin')
+
+if (kv >= 2000 && housePrice == 99999 && currency === 'azn') {document.write('Men evi aliram')}
+else (document.write('men evi almiram'))
---
+++ Lesson 19 Task 1/App.js
@@ -0,0 +1,13 @@
+let name = prompt('ad daxil edin')
+
+switch (name) {
+    case 'user':
+        document.write('Xos geldin istifadeci')
+        break;
+    case 'admin':
+        document.write('Xos geldin Cenab')
+        break;
+    case 'guest':
+        document.write('Xos geldin qonaq')
+        break;
+}
