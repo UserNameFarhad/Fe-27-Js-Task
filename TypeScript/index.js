c174a1f432539a26b943158fc2dcbbe666145175
UserNameFarhad <155009258+UserNameFarhad@users.noreply.github.com>
2024-10-17 20:42:21 +0400

    Add files via upload

---
+++ TypeScript/index.js
@@ -0,0 +1,9 @@
+// let a: number | string = 12
+// a = "ali"
+// a = 67
+// a = NaN
+// console.log('salam');
+var rootEl = document.getElementById("root");
+if (rootEl) {
+    rootEl.append(document.createElement("a"));
+}
