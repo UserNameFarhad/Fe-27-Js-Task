2f3766521eee1e779fe8c2d2e64e0749049509da
UserNameFarhad <fa85619@gmail.com>
2024-11-14 23:37:48 +0400

    React-pizza

--- Eccomerse Website/script.js
+++ Eccomerse Website/script.js
@@ -1,67 +1,69 @@
 let searchform = document.querySelector('.search-form');
 
-document.querySelector('#search-btn').onclick = () => {
+document.querySelector('#search-btn').onclick = () =>{
     searchform.classList.toggle('active');
-    navbar.classList.remove('active')
+    navbar.classList.remove('active');
 }
 
-let navbar = document.querySelector('.navbar')
+let navbar = document.querySelector('.navbar');
 
-document.querySelector('#menu-btn').onclick = () => {
-    navbar.classList.toggle('active')
-    searchform.classList.remove('active')
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchform.classList.remove('active');
 }
 
 let slides = document.querySelectorAll('.home .slides-container .slide');
 let index = 0
 
-function next() {
+function next(){
     slides[index].classList.remove('active');
     index = (index + 1) % slides.length;
     slides[index].classList.add('active');
 }
 
-function prev() {
+function prev(){
     slides[index].classList.remove('active');
     index = (index - 1 + slides.length) % slides.length;
     slides[index].classList.add('active');
 }
 
-window.onscroll = () => {
+window.onscroll = () =>{
     searchform.classList.remove('active');
-    navbar.classList.remove('active'); 2
+    navbar.classList.remove('active');
 
-    if (window.scrollY > 30) {
-        document.querySelector('header').classList.add('header-active')
-    } else {
-        document.querySelector('header').classList.remove('header-active')
+    if(window.scrollY > 30){
+        document.querySelector('header').classList.add('header-active');
     }
+    else{
+        document.querySelector('header').classList.remove('header-active');
+    }
+
 }
 
-let swiper = new Swiper(".featured-slider", {
+var swiper = new Swiper(".featured-slider", {
     loop: true,
     centeredSlides: true,
     spaceBetween: 20,
-    autoPlay: {
+    autoplay:{
         delay: 9500,
-        disableOnInteractive: false,
+        disableOnInteraction:false,
     },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-    breakpoints: {
-        0: {
+    breakpoints:{
+        0:{
             slidesPerView: 1,
         },
-        450: {
+        450:{
             slidesPerView: 2,
         },
-        768: {
+        768:{
             slidesPerView: 3,
         },
-        1200: {
+        1200:{
             slidesPerView: 4,
         },
-    }
-})
+    },
+});
--- TypeScript/index.js
+++
@@ -1,9 +0,0 @@
-// let a: number | string = 12
-// a = "ali"
-// a = 67
-// a = NaN
-// console.log('salam');
-var rootEl = document.getElementById("root");
-if (rootEl) {
-    rootEl.append(document.createElement("a"));
-}
