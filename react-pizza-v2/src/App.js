fa47554465de3b557f9e43c1db3e871ab8d4e553
UserNameFarhad <fa85619@gmail.com>
2024-11-22 21:43:59 +0400

    ...

--- react-pizza-v2/src/App.js
+++ react-pizza-v2/src/App.js
@@ -1,32 +1,25 @@
 import "./App.css";
 import "./scss/app.scss";
 import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
+import Home from "./Pages/Home"
 import pizzas from "./assets/pizzas.json"
+import React from "react";
+import NotFound from "../src/Pages/NotFound"
+import { Route, RouterProvider, Routes } from "react-router-dom";
+import Cart from "./Pages/Cart";
 
 function App() {
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {
-              pizzas.map((obj, id) =>
-                <PizzaBlock
-                  key={id}
-                  {...obj}
-                />
-              )
-            }
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
         </div>
       </div>
     </div>
